78ef487c14b209b8896aaeea5c9dc3a43c8dff0a
MiaElvir <melvirrojas@gmail.com>
2024-01-26 15:57:03 -0600

    inicio de Eliminar

--- src/lab2p2_miaelvir/Lab2P2_MiaElvir.java
+++ src/lab2p2_miaelvir/Lab2P2_MiaElvir.java
@@ -115,6 +115,104 @@ public class Lab2P2_MiaElvir {
                 
                 }else if(Usuarios.get(pos).getTipo_user().equals("bibliotecario")){
                 
+                    System.out.println("--- Menu ---\n1. Listar recursos\n2. Crear recurso\n3.Eliminar Recurso\n4.Modificar recurso\n5. Salir\nIngrese su eleccion: ");
+                    int elec = papoy.nextInt(); 
+                    while (elec > 0 && elec < 5){
+                        switch (elec){
+                            case 1: 
+                                listar(Base); 
+                                break; 
+                            case 2: 
+                                System.out.println("--- Crear ---\n1. Libro\n2. Articulos\n3. Cursos en linea\n4. Conferencias Virtuales\nIngrese su eleccion: "); 
+                                int recurso = papoy.nextInt(); 
+                                switch(recurso){
+                                    case 1: 
+                                        System.out.println("-- Crear Libro\nIngrese el titulo: ");
+                                        String titulo = papoy.next(); 
+                                        System.out.println("Ingrese el autor: ");
+                                        String autor = papoy.next();
+                                        System.out.println("Ingrese el genero: ");
+                                        String genero = papoy.next();
+                                        System.out.println("Ingrese el año de publicacion: ");
+                                        String anio = papoy.next();
+                                        System.out.println("Ingrese la disponibilidad");
+                                        String dispo = papoy.next(); 
+                                        boolean d = false; 
+                                        if (dispo.equalsIgnoreCase("si")){
+                                            d = true; 
+                                        }
+                                        Base.add(new Libro(titulo, autor, genero, anio, d)); 
+                                        break; 
+                                    case 2: 
+                                        System.out.println("-- Crear Articulo\nIngrese el titulo: ");
+                                        titulo = papoy.next(); 
+                                        System.out.println("Ingrese el autor: ");
+                                        autor = papoy.next();
+                                        System.out.println("Ingrese el tema: ");
+                                        String tema = papoy.next();
+                                        System.out.println("Ingrese la fecha de publicacion: ");
+                                        String fechapub = papoy.next();
+                                        System.out.println("Ingrese el acceso en linea: ");
+                                        String acceso = papoy.next(); 
+                                        boolean acc = false; 
+                                        if (acceso.equalsIgnoreCase("si")){
+                                            acc = true; 
+                                        }
+                                        Base.add(new Articulos(titulo, autor, tema, fechapub, acc)); 
+                                        
+                                        break; 
+                                    case 3: 
+                                        System.out.println("-- Crear Curso en Linea\nIngrese el titulo: ");
+                                        titulo = papoy.next(); 
+                                        System.out.println("Ingrese el instructor: ");
+                                        String instructor = papoy.next();
+                                        System.out.println("Ingrese la duracion: ");
+                                        String duracion = papoy.next();
+                                        System.out.println("Ingrese la plataforma de enseñanza: ");
+                                        String plataforma = papoy.next(); 
+                                        Base.add(new CursosEnLinea(titulo, instructor, duracion, plataforma)); 
+                                        break; 
+                                    case 4: 
+                                        System.out.println("-- Crear Conferencia Virtual\nIngrese el titulo: ");
+                                        titulo = papoy.next(); 
+                                        System.out.println("Ingrese el conferencista: ");
+                                        String conferencista = papoy.next();
+                                        System.out.println("Ingrese la fecha: ");
+                                        String fecha = papoy.next();
+                                        System.out.println("Ingrese el enlace de acceso: ");
+                                        String enlace = papoy.next(); 
+                                        Base.add(new ConferenciasVirtuales(titulo, conferencista, fecha, fecha, enlace)); 
+                                        break; 
+                                }
+                                break; 
+                            case 3: 
+                                System.out.println("Ingrese el titulo del recurso o identificador unico que desea eliminar: ");
+                                String titulo = papoy.next(); 
+                                
+                                
+                                break; 
+                                
+                            case 4: 
+                                
+                                break;
+                                
+                            
+                        }
+                        System.out.println("--- Menu ---\n1. Listar recursos\n2. Crear recurso\n3. Salir\nIngrese su eleccion: ");
+                        elec = papoy.nextInt(); 
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                 }
                 
             }else{
@@ -144,7 +242,7 @@ public class Lab2P2_MiaElvir {
     public static void listar(ArrayList base){
         for (int i = 0; i < base.size(); i++) {
             Object recurso = base.get(i); 
-            System.out.println((i+1)+recurso.toString());
+            System.out.println((i+1)+" "+recurso.toString());
         }
     }
    
